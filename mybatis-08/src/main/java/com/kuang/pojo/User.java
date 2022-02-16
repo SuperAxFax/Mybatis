f1d876e05918683f8f27c295d4befc3d317e967d
SuperAx <86346790+SuperAxFax@users.noreply.github.com>
2022-02-16 23:38:57 +0800

    Update User.java

--- mybatis-08/src/main/java/com/kuang/pojo/User.java
+++ mybatis-08/src/main/java/com/kuang/pojo/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable{
     private int id;
     private String name;
     private String pwd;
